ea7274b5e538d3bde7595138ea8aa2fea19a088a
Mashal Hussain <2144169@talnet.nl>
2023-12-01 15:43:16 +0100

    test

--- app/Http/Controllers/PlanetController.php
+++ app/Http/Controllers/PlanetController.php
@@ -2,35 +2,33 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
+
 use App\Models\Planet;
 use Illuminate\Http\Request;
-=======
 use Illuminate\Support\Facades\DB;
->>>>>>> fd24093e6042b128f7028b5a421462523ed16839
+
 
 class PlanetController extends Controller
 {
     public function index()
     {
-<<<<<<< HEAD
+
         // Gebruik Eloquent om alle planeten op te halen
         $planets = Planet::all();
         dd($planets);
-=======
+
         $planets = DB::table('planets')->get();
->>>>>>> fd24093e6042b128f7028b5a421462523ed16839
+
         return view('planets.index', ['planets' => $planets]);
     }
 
     public function show($planet)
     {
-<<<<<<< HEAD
+
         // Gebruik Eloquent om een planeet op te halen op basis van de naam
         $planetData = Planet::where('name', $planet)->first();
-=======
+
         $planetData = DB::table('planets')->where('name', $planet)->first();
->>>>>>> fd24093e6042b128f7028b5a421462523ed16839
 
         if (!$planetData) {
             return redirect('/planets');
