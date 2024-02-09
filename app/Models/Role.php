4eecabfe7606b4f9255e677c969f4d2322252bcd
Mashal Hussain <2144169@talnet.nl>
2024-02-09 10:53:52 +0100

    your message

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
     use HasFactory;
+
+    public function users ( ) :BelongsToMany
+     {
+                return $this->belongsToMany(User::class)
+                ->withPivot (' status ') ->withTimestamps ();
+     }
 }
